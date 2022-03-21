03295f42521ad94b66aeb957746c6158ef1a6f56
Prasanna.P <Prasanna.P@EXIND3020-70.allfon.in>
2022-03-21 12:47:15 +0530

    REQ 102 :- get by dept endpoint added

--- jenkins-git-maven-docker-build/src/main/java/com/example/demo/controller/TeacherController.java
+++ jenkins-git-maven-docker-build/src/main/java/com/example/demo/controller/TeacherController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,15 @@ public class TeacherController {
 	@GetMapping("/teachers")
 	public String getTeacher() {
 		List<String> list =Arrays.asList("amar","akbar","antony");
+		return list.toString();
+	}
+	
+	@GetMapping("/teachers/{dept}")
+	public String getTeacherByDept(@PathVariable("dept") String dept) {
+		List<String> list =Arrays.asList("amar","akbar","antony");
+		if (dept.equals("ece")) {
+			list = Arrays.asList("shankar","salim","simaon");
+		}
 		return list.toString();
 	}
 }
